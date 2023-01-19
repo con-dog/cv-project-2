c6efcb121cc7744b2bace03a8ca16cdc2d1c5d6e
Connor <connordanielfrank@gmail.com>
2023-01-20 12:34:21 +1300

    build: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [mode, setMode] = useState('Edit');
+
+  const toggleMode = () => {
+    if (mode === 'Edit') {
+      setMode('Generate');
+    } else {
+      setMode('Edit');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleMode}>{mode}</button>
     </div>
   );
-}
+};
 
 export default App;
