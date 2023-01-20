46354afa74819804890a49d120339e43c4c7352f
Connor <connordanielfrank@gmail.com>
2023-01-20 15:43:01 +1300

    chore: Update eslint etc

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import CV from './components/CV';
 import Form from './components/Form';
 
 function App() {
-  const [isEdit, setMode] = useState(true);
+  const [isEditing, setMode] = useState(true);
 
   const toggleMode = () => {
-    setMode(!isEdit);
+    setMode(!isEditing);
   };
 
   return (
     <div>
-      <button type="button" onClick={toggleMode}>{isEdit ? 'Generate' : 'Edit'}</button>
-      {isEdit ? <Form /> : <CV />}
+      <button type="button" onClick={toggleMode}>{isEditing ? 'Generate' : 'Edit'}</button>
+      {isEditing ? <Form /> : <CV />}
     </div>
   );
 }
